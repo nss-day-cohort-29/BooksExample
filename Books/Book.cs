221a39ea34d8d86612973223f4eee66be68e74ab
Andy Collins <andy.collins@relode.com>
2019-03-20 10:35:33 -0500

    initial commit

---
+++ Books/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    public class Book
+    {
+        public string Title { get; }
+
+        private int _pageCount;
+        public int PageCount
+        {
+            get
+            {
+                return this._pageCount;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this._pageCount = value;
+                }
+            }
+        }
+        public string Author { get; set; }
+
+        public Book(string title, int pageCount)
+        {
+            this.Title = title;
+            this.PageCount = pageCount;
+        }
+    }
+}
--- Books/Program.cs
+++ Books/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Books
 {
@@ -6,7 +8,90 @@ namespace Books
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book tomBook = new Book("Adventures of Tom Sawyer", 300);
+
+            string title = tomBook.Title;
+            int pageCount = tomBook.PageCount;
+            string author = tomBook.Author;
+
+            //tomBook.Title = "Huck Finn";
+
+            tomBook.PageCount = 400;
+            tomBook.PageCount = -27;
+
+            Console.WriteLine($"The author is {author}");
+
+
+
+            List<string> titles = new List<string>
+            {
+                "Official Book Club Selection",
+                "Are You There, Vodka? It's Me, Chelsea",
+                "When You Look Like Your Passport Photo, It's Time to Go Home",
+                "Motherhood: The Second Oldest Profession",
+                "I Owe Russia $1200",
+                "Everything You Always Wanted to Know About Sex(But Were Afraid to Ask)",
+                "Too Fat to Fish",
+                "Politically Correct Bedtime Stories",
+                "How to Teach Physics to Your Dog Intriguing",
+                "Liberal Fascism",
+                "People Who Eat Darkness",
+                "Mine Enemy Grows Older",
+                "Your Erroneous Zones",
+                "Barbarians at the Gate",
+                "Women Who Run With the Wolves",
+                "Midnight In the Garden of Good and Evil",
+                "The Devil In the White City",
+                "Hot, Flat, and Crowded",
+                "This Side of Innocence",
+                "East of Eden",
+                "The View from Pompey's Head",
+                "Something of Value",
+                "Ship of Fools",
+                "The Spy Who Came in from the Cold",
+                "Valley of the Dolls",
+                "Looking for Mr.Goodbar",
+                "How Stella Got Her Groove Back",
+                "The Deep End of the Ocean",
+                "Pretend You Don't See Her ",
+            };
+
+            /*
+            List<Book> books = new List<Book>();
+            foreach (string t in titles)
+            {
+                books.Add(new Book(t, 27));
+            }
+            */
+
+            List<Book> books = titles
+                .Select(t => new Book(t, t.Length * 100))
+                .Where(b => b.PageCount > 2000)
+                .OrderBy(taco => taco.Title)
+                .ToList();
+
+            foreach (string t in titles)
+            {
+                Console.WriteLine(t);
+            }
+
+            foreach (Book b in books)
+            {
+                Console.WriteLine($"Title: {b.Title}, PageCount: {b.PageCount}");
+            }
+
+
+            List<Book> books1 = (
+                from t in titles
+                select new Book(t, t.Length * 100)
+            ).ToList();
+
+            List<Book> filteredBooks = (from b in books1
+                                        where b.PageCount > 2000
+                                        orderby b.Title
+                                        select b).ToList();
+
+            Console.ReadKey();
         }
     }
 }
